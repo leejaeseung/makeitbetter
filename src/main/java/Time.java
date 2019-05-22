20d49e2c7f2ecc0bdb85ee58f80b2c86556ed318
leejaeseung <wasd222@naver.com>
2019-05-22 19:20:34 +0900

    ho2

--- src/main/java/Time.java
+++
@@ -1,9 +0,0 @@
-public class mainclass {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("hello world!");
-	System.out.println("hello world!");
-    }
-
-
-}
